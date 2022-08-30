81bfe6c78af367904a209af6da37fdfc54f9a40c
Melquisedeque <melck_junior@hotmail.com>
2022-08-30 18:07:47 +0100

    Navbar styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <h1>Hello</h1>
+    </>
   );
 }
 
