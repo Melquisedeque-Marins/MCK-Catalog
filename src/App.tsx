5ad1caf4a9a97cbea85137603b77b1595d4950ad
Melquisedeque <melck_junior@hotmail.com>
2022-08-31 16:03:09 +0100

    Home styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
+import Home from 'pages/Home';
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <h1>Hello</h1>
-    </>
+    <Home/>
   );
 }
 
---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const ButtonIcon = () => {
+    return (
+        
+        <button className="btn btn-primary btn-icon">
+            <h6>INICIE AGORA SUA BUSCA</h6>
+        </button>
+    );
+}
+
+export default ButtonIcon;
