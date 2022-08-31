b4f31f13ae60ae99c15dd3fe2ebc30790528a71e
Melquisedeque <melck_junior@hotmail.com>
2022-08-31 18:34:44 +0100

    Custom ButtomIcon

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,12 +1,16 @@
 import './styles.css';
-
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 const ButtonIcon = () => {
-    return (
-        
-        <button className="btn btn-primary btn-icon">
-            <h6>INICIE AGORA SUA BUSCA</h6>
-        </button>
-    );
-}
+  return (
+    <div className="btn-container">
+          <button className="btn btn-primary ">
+            <h6>Inicie agora a sua busca</h6>
+          </button>
+        <div className="btn-icon-container">
+            <ArrowIcon />
+        </div>
+    </div>
+  );
+};
 
 export default ButtonIcon;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,31 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
+
 import { Link } from 'react-router-dom';
 import './styles.css';
 const Home = () => {
   return (
-      <div className="home-container">
-        <div className="base-card home-card">
-            <div className="home-content-container">
-                <div>
-                    <h1>Conheça o melhor catálogo de produtos</h1>
-                    <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
-                </div>
-                <Link to="/products">
-                    <ButtonIcon />
-                </Link>
-            </div>
-            <div className="home-image-container">
-                <MainImage/>
-            </div>
+    <div className="home-container">
+      <div className="base-card home-card">
+        <div className="home-content-container">
+          <div>
+            <h1>Conheça o melhor catálogo de produtos</h1>
+            <p>
+              Ajudaremos você a encontrar os melhores produtos disponíveis no
+              mercado.
+            </p>
+          </div>
+          <div>
+            <Link to="/products">
+                <ButtonIcon />
+            </Link>
+          </div>
+        </div>
+        <div className="home-image-container">
+          <MainImage />
         </div>
       </div>
+    </div>
   );
 };
 
