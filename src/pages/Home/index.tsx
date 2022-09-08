73d767292667beed1c91aefae632a3e548bf5fe9
Melquisedeque <melck_junior@hotmail.com>
2022-09-08 14:22:12 +0100

    Props

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,7 +17,7 @@ const Home = () => {
           </div>
           <div>
             <Link to="/products">
-                <ButtonIcon />
+                <ButtonIcon text="Inicie agora a sua pesquisa"/>
             </Link>
           </div>
         </div>
