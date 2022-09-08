ffd187021fceebbb1361d1671432072a7c36c4c7
Melquisedeque <melck_junior@hotmail.com>
2022-09-08 16:11:55 +0100

    ProductDetailes layout mobile

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -1,8 +1,40 @@
-const ProducDetails = () => {
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+import './styles.css';
 
-    return(
-        <h1>product details</h1>
-    );
-}
+const ProductDetails = () => {
+  return (
+    <div className="product-details-container">
+      <div className="base-card product-details-card">
+        <div className="goback-container">
+          <ArrowIcon />
+          <h2>VOLTAR</h2>
+        </div>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+                alt="nome do produto"
+              />
+            </div>
+            <div className="name-price-container">
+              <h1>Nome do produto</h1>
+              <ProductPrice price={2345.67} />
+            </div>
+          </div>
+            <div className="col-xl-6">
+              <div className="description-container">
+                <h2>Descrição do produto</h2>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Nemo, nulla.
+                </p>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ProducDetails;
+export default ProductDetails;
