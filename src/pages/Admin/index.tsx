5a44c33f2ee652e539752d5ca529157a121310e7
Melquisedeque <melck_junior@hotmail.com>
2022-09-27 16:06:03 +0100

    Admin navbar layout mobile

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,8 +1,14 @@
-
+import Navbar from "./Navbar";
+import './styles.css';
 const Admin = () => {
 
     return (
-        <h1>Pagina admin</h1>
+        <div className="admin-container">
+            <Navbar />
+            <div className="admin-content">
+                <h1>Conteudo</h1>
+            </div>
+        </div>
     );
 }
 
