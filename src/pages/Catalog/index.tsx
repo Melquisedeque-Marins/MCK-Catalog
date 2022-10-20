52c23d45288718f87930d0a750ac8a8878506b0a
Melquisedeque <melck_junior@hotmail.com>
2022-10-20 15:51:01 +0100

    Authorized request

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,11 +1,11 @@
-import axios, { AxiosRequestConfig } from "axios";
+import { AxiosRequestConfig } from "axios";
 import Pagination from "components/Pagination";
 import ProductCard from "components/ProductCard";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Product } from "types/products";
 import { SpringPage } from "types/vendor/spring";
-import { BASE_URL } from "util/requests";
+import { requestBackend } from "util/requests";
 import CardLoader from "./CardLoader";
 import './styles.css';
 
@@ -18,14 +18,13 @@ const Catalog = () => {
         const params : AxiosRequestConfig = {
             method: "GET",
             url: "/products",
-            baseURL: BASE_URL,
             params: {
                 page: 0,
                 size: 12
             }
         }
         setIsLoading(true);
-        axios(params)
+        requestBackend(params)
             .then(response => {
                 setPage(response.data);
             })
--- src/util/requests.ts
+++ src/util/requests.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import qs from "qs";
 
 type LoginResponse = {
@@ -37,6 +37,15 @@ export const requestBackendLogin = (loginData : LoginData) => {
     });
 
     return axios({method: 'POST', baseURL: BASE_URL, url: '/oauth/token', data, headers});
+}
+
+export const requestBackend = (config: AxiosRequestConfig) => {
+
+    const headers = config.withCredentials ? {
+        ...config.headers, Authorization: 'Bearer ' + getAuthData().access_token,
+    } : config.headers;
+
+    return axios({...config, baseURL: BASE_URL, headers});
 }
 
 export const saveAuthData = (obj : LoginResponse) => {
