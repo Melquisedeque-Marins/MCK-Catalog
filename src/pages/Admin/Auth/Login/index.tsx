9b5a740325836fed0a1ef9f0beb10d35c33406af
Melquisedeque <melck_junior@hotmail.com>
2022-10-20 14:11:21 +0100

    Login request

--- src/pages/Admin/Auth/Login/index.tsx
+++ src/pages/Admin/Auth/Login/index.tsx
@@ -3,6 +3,7 @@ import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from "react-hook-form";
 
 import './styles.css';
+import { requestBackendLogin } from 'util/requests';
 
 type FormData = {
   username : string;
@@ -14,8 +15,14 @@ const Login = () => {
   const { register, handleSubmit } = useForm<FormData>();
 
   const onSubmit = (formData : FormData) => {
-    console.log(formData);
-  }
+    requestBackendLogin(formData)
+    .then(response => {
+      console.log('SUCESSO', response);
+    })
+    .catch(error => {
+      console.log('ERRO', error);
+    });
+  };
 
   return (
     <div className="base-card login-card">
--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,10 +1,9 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import Pagination from "components/Pagination";
 import ProductCard from "components/ProductCard";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Product } from "types/products";
-import { AxiosParams } from "types/vendor/axios";
 import { SpringPage } from "types/vendor/spring";
 import { BASE_URL } from "util/requests";
 import CardLoader from "./CardLoader";
@@ -16,9 +15,10 @@ const Catalog = () => {
 
     useEffect(() => {
 
-        const params : AxiosParams = {
+        const params : AxiosRequestConfig = {
             method: "GET",
-            url: `${BASE_URL}/products`,
+            url: "/products",
+            baseURL: BASE_URL,
             params: {
                 page: 0,
                 size: 12
