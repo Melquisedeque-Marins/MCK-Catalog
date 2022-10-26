a61b4f8b433683a716e6fdd0b55682e2be74315d
Melquisedeque <melck_junior@hotmail.com>
2022-10-26 15:15:49 +0100

    UI restriction on user roles

--- src/pages/Admin/Navbar/index.tsx
+++ src/pages/Admin/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import { hasAnyHoles } from 'util/requests';
 import './styles.css';
 
 const Navbar = ()=> {
@@ -15,11 +16,13 @@ const Navbar = ()=> {
                         <p>Categorias</p>
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to="/admin/users" className="admin-nav-item">
-                        <p>Usuários</p>
-                    </NavLink>
-                </li>
+               {hasAnyHoles(['ROLE_ADMIN']) &&
+               (  <li>
+                 <NavLink to="/admin/users" className="admin-nav-item">
+                     <p>Usuários</p>
+                 </NavLink>
+             </li>)
+               }
             </ul>
         </nav>
     );
--- src/util/requests.ts
+++ src/util/requests.ts
@@ -97,3 +97,24 @@ export const isAuthenticated = () : boolean => {
     const tokenData = getTokenData();
     return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
 }
+
+export const hasAnyHoles = ( roles: Role[] ) : boolean => {
+
+    if (roles.length === 0) {
+        return true;
+    }
+
+    const tokenData = getTokenData();
+
+    
+    if (tokenData !== undefined){
+        for (var i = 0; i < roles.length; i++) {
+            if (tokenData.authorities.includes(roles[i])) {
+                return true;
+            }
+        }
+    }
+    // return roles.some(role => tokenData.authorities.includes(role));
+
+    return false;
+}
