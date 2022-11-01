046a45b8bbf93e51f798ba2aa81929a73c460d3b
Melquisedeque <melck_junior@hotmail.com>
2022-11-01 11:25:10 +0000

    Editing products

--- src/pages/Admin/Products/Form/index.tsx
+++ src/pages/Admin/Products/Form/index.tsx
@@ -1,24 +1,50 @@
 import { AxiosRequestConfig } from 'axios';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { Product } from 'types/products';
+import { setConstantValue } from 'typescript';
 import { requestBackend } from 'util/requests';
 import './styles.css';
 
+type UrlParams = {
+    productId: string;
+}
+
 const Form = () => {
+
+    const { productId } = useParams<UrlParams>();
+
+    const isEditing = productId !== 'create';
 
     const history = useHistory();
 
-    const { register, handleSubmit, formState: { errors } } = useForm<Product>();
+    const { register, handleSubmit, formState: { errors }, setValue } = useForm<Product>();
+
+    useEffect(() => {
+        if (isEditing) {
+            requestBackend({ url: `/products/${productId}`})
+            .then((response) => {
+                const product = response.data as Product;
+
+                setValue('name', product.name);
+                setValue('price', product.price);
+                setValue('description', product.description);
+                setValue('imgUrl', product.imgUrl);
+                setValue('categories', product.categories);
+            });
+        }
+    },[isEditing, productId, setValue]);
 
     const onSubmit = (formData: Product) => {
         const data = { ...formData,
-            imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/9-big.jpg",
-            categories: [ { id: 1, name:""}]}
+            imgUrl: isEditing ? formData.imgUrl : "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/9-big.jpg",
+            categories: isEditing ? formData.categories : [{ id: 1, name:""}] 
+        }
 
         const config : AxiosRequestConfig = {
-            method: "POST",
-            url: "/products",
+            method: isEditing ? 'PUT' : 'POST',
+            url: isEditing ? `/products/${productId}` : '/products',
             data,
             withCredentials: true
         };
